158fa952010d7cabaca349d5ecdbecdfb42dad4f
Alexey Zhivin <aleksey_zhivin@mail.ru>
2023-12-03 23:00:21 +0300

    added circuit and resistor structures, for now they do nothing

--- Project.cpp
+++ Project.cpp
@@ -7,6 +7,7 @@ struct Node;
 
 struct Bar {
     private:
+        double cur, vol;
         Node* startNode;
         Node* endNode;
         std::string name;
@@ -19,13 +20,34 @@ struct Bar {
             name = name_;
         }
 
-        Node* get_nodes() {
-            return startNode, endNode;
+        Node* get_startNode() {
+            return startNode;
+        }
+
+        Node* get_endNode() {
+            return endNode;
         }
 
         Bar(Node* start, Node* end) {
             startNode = start;
             endNode = end;
+        }
+
+        void set_vol(double vol_) {
+            vol = vol_;
+
+        }
+
+        void set_cur(double cur_) {
+            cur = cur_;
+        }
+
+        double get_vol() {
+            return vol;
+        }
+
+        double get_cur() {
+            return cur;
         }
 
         void print_name() {
@@ -39,6 +61,10 @@ struct Bar {
         void connect_end(Node* node) {
             this->endNode = node;
         }
+
+        virtual double vac(double voltage) { //returns current depending on voltage
+            return 0;
+        }
 };
 
 struct Node {
@@ -47,9 +73,24 @@ struct Node {
         std::vector<Bar*> outs;
         unsigned int in_num = 0;
         unsigned int out_num = 0;
+        double potential;
 
     public:
-        Node() {}
+        void set_potential(double potential_){
+            potential = potential_;
+        }
+
+        double get_potential() {
+            return potential; 
+        }
+
+        std::vector<Bar*> get_ins() {
+            return ins;
+        }
+
+        std::vector<Bar*> get_outs() {
+            return outs;
+        }
 
         void connect(Bar *&element, char side){
             if (side == 'o'){
@@ -82,31 +123,86 @@ struct Node {
 
         void print(){
             std::cout << "This is a node with " << in_num << " ins and " << out_num << " outs.\n" << "ins:" << std::endl;
-
             for (size_t i = 0; i != ins.size(); ++i) {
                 ins[i]->print_name();
             }
         }
 };
 
+struct Resistor : Bar
+{
+    private:
+        double res;
+
+    public:
+        Resistor(double res_, std::string name) : Bar(name) {
+            res = res_;
+        }
+
+        void vac() {
+            this->set_vol(this->get_startNode()->get_potential() - this->get_endNode()->get_potential());
+            this->set_cur(this->get_vol() / this->res);
+        }
+
+        double get_res() {
+            return res;
+        }
+};
+
+struct Circuit {
+    private:
+
+        std::vector<Node*> nodes;
+        std::vector<Bar*> bars;
+    
+    public:
+
+        void print() {
+            std::cout << "This is a circuit with following elements" << std::endl;
+            for (size_t i = 0; i != bars.size(); ++i) {
+                bars[i]->print_name();
+            }
+        }
+
+        void add_node(Node*& node) {
+            nodes.push_back(node);
+        }
+
+        void add_bar(Bar*& bar) {
+            bars.push_back(bar);
+        }
+
+        void create () {
+            
+        }
+};
 
 
 int main() {
-    Node a;
+    Node* a = new Node;
     Bar* r1 = new Bar("resistor");
     Bar* r2 = new Bar("capacitor");
     Bar* r3 = new Bar("battery");
     Bar* r4 = new Bar("diode");
 
-    a.connect(r1, 'i');
-    a.connect(r2, 'i');
-    a.connect(r3, 'i');
-    a.connect(r4, 'o');
-    a.print();
-    a.disconnect(r2);
+    a->connect(r1, 'i');
+    a->connect(r2, 'i');
+    a->connect(r3, 'i');
+    a->connect(r4, 'o');
+    a->print();
+    a->disconnect(r2);
     std::cout << std::endl;
-    a.print();
+    a->print();
 
-    delete r1, r2, r3, r4;
+    Circuit c1;
+
+    c1.add_node(a);
+    c1.add_node(a);
+    c1.add_node(a);
+    c1.add_node(a);
+
+    c1.print();
+
+    delete r1, r2, r3, r4, a;
     return 0;
 }
