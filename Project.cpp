dbab874fd5b6e075b6b0cfefcd2ab9e19cdec314
Ярослав Васильев <yaroliss@outlook.com>
2023-12-08 23:13:05 +0300

    new files

--- Project.cpp
+++ Project.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cmath>
 #include <math.h>
+#include <bar.h>
 
 # define PI 3.1415926535897932384626433832795
 
@@ -15,8 +16,6 @@ void print_matrix(std::vector<std::vector<double>> matrix) {
         std::cout << std::endl;
     }
 }
-
-// something new new
 
 std::vector<double> gauss_solve(std::vector<std::vector<double>> matrix) {
     size_t checksum = 0;
@@ -113,75 +112,6 @@ void gauss_solve_test() {
 
 
 struct Node;
-
-struct Bar {
-    private:
-        double cur, vol;
-        Node* startNode;
-        Node* endNode;
-        std::string name;
-
-    public:
-
-        Bar(std::string name_) {
-            startNode = nullptr;
-            endNode = nullptr;
-            name = name_;
-        }
-
-        Node* get_startNode() {
-            return startNode;
-        }
-
-        Node* get_endNode() {
-            return endNode;
-        }
-
-        Bar(Node* start, Node* end) {
-            startNode = start;
-            endNode = end;
-        }
-
-        void set_vol(double vol_) {
-            vol = vol_;
-
-        }
-
-        void set_cur(double cur_) {
-            cur = cur_;
-        }
-
-        double get_vol() {
-            return vol;
-        }
-
-        double get_cur() {
-            return cur;
-        }
-
-        void print_name() {
-            std::cout << name << std::endl;
-        }
-
-        void connect_start(Node* node) {
-            this->startNode = node;
-        }
-
-        void connect_end(Node* node) {
-            this->endNode = node;
-        }
-
-        virtual void vac(double time, double step) { //returns current depending on voltage
-        }
-
-        virtual char get_type() {
-            return 'N';
-        }
-
-        virtual double get_res() {
-            return 0;
-        }
-};
 
 struct Node {
     private:
